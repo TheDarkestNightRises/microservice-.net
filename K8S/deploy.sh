ed2f5e271b0ca734afcf564c4334ed563e013a29
TheDarkestNightRises <icecold363@gmail.com>
2023-03-05 21:27:53 +0100

    Fixed a bug regarding platform service container

--- K8S/deploy.sh
+++ K8S/deploy.sh
@@ -7,7 +7,7 @@ kubectl apply -f platforms-np-srv.yaml
 #Ingress nginx
 kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/controller-v1.6.4/deploy/static/provider/cloud/deploy.yaml
 kubectl apply -f ingress-srv.yaml
-#Mysql
+#Mssql
 kubectl apply -f local-pvc.yaml
 kubectl apply -f mssql-plat-depl.yaml
 # Dont forget to kubectl create secret generic mssql --from-literal=SA_PASSWORD="password" 
