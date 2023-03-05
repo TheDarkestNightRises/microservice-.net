3f3e1574773e2e05aa1caf8136b54bef3b7ae026
TheDarkestNightRises <icecold363@gmail.com>
2023-03-05 19:33:21 +0100

    Made deployment for sql server and local pvc , deploy bash

---
+++ K8S/deploy.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+kubectl apply -f commands-depl.yaml
+kubectl apply -f platforms-depl.yaml
+kubectl apply -f platforms-np-srv.yaml
+kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/controller-v1.6.4/deploy/static/provider/cloud/deploy.yaml
+kubectl apply -f ingress-srv.yaml
+kubectl apply -f local-pvc.yaml
+kubectl apply -f mssql-plat-depl.yaml
+# Dont forget to kubectl create secret generic mssql --from-literal=SA_PASSWORD="password" 
