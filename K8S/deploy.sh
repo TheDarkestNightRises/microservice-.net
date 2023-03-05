1778d8c9e0ea7a2b3bfe264b844338c0c7fae7c3
TheDarkestNightRises <icecold363@gmail.com>
2023-03-05 19:47:04 +0100

    Made destroy deploy

--- K8S/deploy.sh
+++ K8S/deploy.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
+#Services
 kubectl apply -f commands-depl.yaml
 kubectl apply -f platforms-depl.yaml
 kubectl apply -f platforms-np-srv.yaml
+#Ingress nginx
 kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/controller-v1.6.4/deploy/static/provider/cloud/deploy.yaml
 kubectl apply -f ingress-srv.yaml
+#Mysql
 kubectl apply -f local-pvc.yaml
 kubectl apply -f mssql-plat-depl.yaml
 # Dont forget to kubectl create secret generic mssql --from-literal=SA_PASSWORD="password" 
