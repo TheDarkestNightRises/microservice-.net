43a4a5df006333ccc5bd17cc00f627cbc1330722
TheDarkestNightRises <icecold363@gmail.com>
2023-04-06 20:18:12 +0200

    Made Dto for PlatformPublished

--- K8S/destroy-deploy.sh
+++ K8S/destroy-deploy.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
-
+#Deployment
 kubectl delete deployment commands-depl
 kubectl delete deployment mssql-depl
 kubectl delete deployment platforms-depl
-kubectl delete deployment rabbitmq-depl.yaml
+kubectl delete deployment rabbitmq-depl
+#Services
 kubectl delete services commands-clusterip-srv
 kubectl delete service platforms-clusterip-srv
 kubectl delete service mssql-clusterip-srv
 kubectl delete service platformnpservice-srv
 kubectl delete service rabbitmq-clusterip-srv
+#LoadBalancers
 kubectl delete service mssql-loadbalancer
 kubectl delete service rabbitmq-loadbalancer
 kubectl delete deployment ingress-nginx-controller -n ingress-nginx
--- PlatformService/Profiles/PlatformsProfile.cs
+++ PlatformService/Profiles/PlatformsProfile.cs
@@ -10,5 +10,6 @@ public class PlatformsProfile : Profile
         //Source --> Target
         CreateMap<Platform, PlatformReadDto>();
         CreateMap<PlatformCreateDto, Platform>();
+        CreateMap<PlatformReadDto, PlatformPublishedDto>();
     }
 }
